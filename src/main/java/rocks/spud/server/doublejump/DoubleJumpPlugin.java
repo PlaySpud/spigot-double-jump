5ad24cfea8ef3f97638d487394f00ae03c5e8235
Johannes Donath <johannesd@torchmind.com>
2015-02-23 09:27:02 +0100

    Optimized cooldown handling further

--- src/main/java/rocks/spud/server/doublejump/DoubleJumpPlugin.java
+++ src/main/java/rocks/spud/server/doublejump/DoubleJumpPlugin.java
@@ -1,6 +1,7 @@
 package rocks.spud.server.doublejump;
 
 import org.bukkit.GameMode;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -10,23 +11,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Provides a simple plugin for allowing players to double jump.
  * @author {@literal Johannes Donath <johannesd@torchmind.com>}
  */
 public class DoubleJumpPlugin extends JavaPlugin implements Listener {
-
-	/**
-	 * Stores a list of players who used their double jump already.
-	 */
-	private List<Player> playerList = new ArrayList<> ();
 
 	/**
 	 * {@inheritDoc}
@@ -39,13 +31,17 @@ public class DoubleJumpPlugin extends JavaPlugin implements Listener {
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Performs an update on the player flight state.
+	 * @param player The player.
 	 */
-	@Override
-	public void onDisable () {
-		super.onDisable ();
+	private void groundUpdate (Player player) {
+		Location location = player.getPlayer ().getLocation ();
+		location = location.subtract (0, 1, 0);
 
-		this.playerList.clear ();
+		Block block = location.getBlock ();
+		if (!block.getType ().isSolid ()) return;
+
+		player.setAllowFlight (true);
 	}
 
 	/**
@@ -54,16 +50,7 @@ public class DoubleJumpPlugin extends JavaPlugin implements Listener {
 	 */
 	@EventHandler (priority = EventPriority.HIGH)
 	public void onPlayerJoin (PlayerJoinEvent event) {
-		event.getPlayer ().setAllowFlight (true);
-	}
-
-	/**
-	 * Handles player disconnects.
-	 * @param event The event.
-	 */
-	@EventHandler (priority = EventPriority.HIGH)
-	public void onPlayerQuit (PlayerQuitEvent event) {
-		this.playerList.remove (event.getPlayer ());
+		this.groundUpdate (event.getPlayer ());
 	}
 
 	/**
@@ -84,15 +71,7 @@ public class DoubleJumpPlugin extends JavaPlugin implements Listener {
 	@EventHandler (priority = EventPriority.HIGH)
 	public void onPlayerMove (PlayerMoveEvent event) {
 		if (event.getPlayer ().getAllowFlight ()) return;
-
-		if (this.playerList.contains (event.getPlayer ())) {
-			Block block = event.getPlayer ().getLocation ().subtract (0, 1, 0).getBlock ();
-
-			if (block.getType ().isSolid ()) {
-				this.playerList.remove (event.getPlayer ());
-				event.getPlayer ().setAllowFlight (true);
-			}
-		}
+		this.groundUpdate (event.getPlayer ());
 	}
 
 	/**
@@ -105,8 +84,6 @@ public class DoubleJumpPlugin extends JavaPlugin implements Listener {
 
 		event.setCancelled (true);
 		event.getPlayer ().setAllowFlight (false);
-		this.playerList.add (event.getPlayer ());
-
 		event.getPlayer ().setVelocity (event.getPlayer ().getLocation ().getDirection ().multiply (1.6d).setY (1.0d));
 	}
 }
